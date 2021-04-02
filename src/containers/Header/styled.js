0c394c1c4488793fa72c793104ca1184adf1825d
Camila Moura <camila.moura@enext.com.br>
2021-04-02 17:40:10 -0300

    added styled components and header

---
+++ src/containers/Header/styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  z-index: 1;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+
+  p {
+    font-weight: 300;
+    text-transform: uppercase;
+    font-size: 0.875rem;
+    margin-left: 0.5rem;
+  }
+
+  .menu {
+    margin-left: 1.75rem;
+  }
+`;
