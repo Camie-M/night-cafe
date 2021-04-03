92c3dec2cd2bf66da2c0cca7d0652216f5333e6b
Camila Moura <camila.moura@enext.com.br>
2021-04-03 14:57:34 -0300

    added description table mq

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 import Header from './containers/Header';
 import MainTitle from './containers/MainTitle';
-/* import DescriptionTable from './containers/DescriptionTable';
-import FirstImageGallery from './containers/FirstImageGallery';
+import DescriptionTable from './containers/DescriptionTable';
+/* import FirstImageGallery from './containers/FirstImageGallery';
 import SecondImageGallery from './containers/SecondImageGallery';
 import Navigation from './containers/Navigation';
 import Footer from './containers/Footer'; */
@@ -13,8 +13,8 @@ function App() {
     <>
       <Header />
       <MainTitle />
-      {/* <DescriptionTable />
-      <FirstImageGallery />
+      <DescriptionTable />
+      {/* <FirstImageGallery />
       <SecondImageGallery />
       <Navigation />
       <Footer /> */}
--- src/containers/DescriptionTable/components/Description/styled.js
+++ src/containers/DescriptionTable/components/Description/styled.js
@@ -2,4 +2,10 @@ import styled from 'styled-components';
 
 export const DescriptionWrapper = styled.div`
   width: 60%;
+
+  @media(max-width: 900px) {
+    width: 80%;
+    text-align: center;
+    margin-top: 2.5rem;
+  }
 `;
